6d6970a7584b42166ecccdf74055974a683351c5
Deportator <fabrichnov87@gmail.com>
2022-10-03 22:58:48 +0300

    fixed task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,13 @@
-use strict";
+"use strict";
 function solveEquation(a, b, c) {
-  const arr = [];
-  const discriminant = Math.pow(b, 2) - 4 * a * c; 
-  if (discriminant > 0) {   
-    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    arr.push(x1, x2);
+  const discriminant = b**2 - 4 * a * c;
+  if (discriminant > 0){
+  return [(- b + Math.sqrt(discriminant)) / (2 * a), (- b - Math.sqrt(discriminant)) / (2 * a)];
+  } else if (discriminant === 0) {
+  return [- b / (2 * a)];
   }
-  else if (discriminant === 0) {   
-    let x1 = -b / (2 * a);
-    arr.push(x1);
+  return [];
   }
-  else if (discriminant < 0) {  
-  }
-  
-  return arr; // array
-}
 
 /*function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
