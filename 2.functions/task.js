d7c83a814c284ed8ff61018fb49fae691e8421c1
Deportator <fabrichnov87@gmail.com>
2022-10-04 23:39:59 +0300

    reformat code

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -6,10 +6,12 @@ function getArrayParams(arr) {
   max = -Infinity;
   sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max)
-      max = arr[i];
-    if (arr[i] < min)
-      min = arr[i];
+    if (arr[i] > max) {
+      max = arr[i]; 
+    }
+    if (arr[i] < min) {
+      min = arr[i]; 
+    }
     sum = arr[i] + sum;
   }
   avg = Number((sum / arr.length).toFixed(2));
